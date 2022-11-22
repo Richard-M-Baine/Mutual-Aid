90f868d4865ebf5717c49a10c7624d4ca016fc6f
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-11-22 15:29:25 -0500

    group by id

--- app/api/group_routes.py
+++ app/api/group_routes.py
@@ -8,10 +8,18 @@ from app.models import User, Groups, db
 group_routes = Blueprint('groups', __name__ )
 
 
-
+# get all groups
 @group_routes.route('/all')
 
 def get_all_groups():
     groups = Groups.query.all()
     response = {"groups": [group.to_dict() for group in groups]}
     return make_response(response, 200)
+
+
+# get group by id
+
+@group_routes.route("/<int:id>")
+def single_group(id):
+  single_group = Groups.query.get(id)
+  return make_response(single_group.to_dict(), 200)

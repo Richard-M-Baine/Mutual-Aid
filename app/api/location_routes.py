0ad9fd30a62a6900f28d3846778592ba3f496f73
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-11-23 11:19:23 -0500

    by id for locations

--- app/api/location_routes.py
+++ app/api/location_routes.py
@@ -12,9 +12,19 @@ location_routes = Blueprint('location', __name__)
 
 @location_routes.route('/all')
 
-
+# all locations probably wont need
 def get_all_locations():
 
     locations = Locations.query.all()
     response = {"locations": [location.to_dict() for location in locations]}
     return make_response(response, 200)
+
+
+# one location
+@location_routes.route("/<int:id>")
+
+def single_group(id):
+  single_location = Locations.query.get(id)
+  
+  
+  return make_response(single_location.to_dict(), 200)

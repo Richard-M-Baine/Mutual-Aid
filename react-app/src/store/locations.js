19a075e2d52d93498defffd5739897cb86d3112b
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-11-23 12:28:01 -0500

    store updated for locations not tested

---
+++ react-app/src/store/locations.js
@@ -0,0 +1,91 @@
+
+const ALL_LOCATIONS ='locations/all'
+const ONE_LOCATION = 'locations/one'
+
+
+
+const getAllLocationsAction = payload => {
+
+    return {
+        type: ALL_LOCATIONS,
+        payload
+    }
+}
+
+const getOneLocationAction = payload => {
+
+    return {
+        type: ONE_LOCATION,
+        payload
+    }
+}
+
+// thunkville
+
+
+// all groups
+export const fetchAllLocationsThunk = () => async dispatch => {
+
+    const response = await fetch('/api/locations/all')
+
+    if (response.ok) {
+
+        const locations = await response.json()
+
+        dispatch(getAllLocationsAction(locations))
+
+        return locations
+    }
+
+}
+
+// one group
+
+export const getOneLocationsThunk = id => async dispatch => {
+    
+    const res = await fetch(`/api/locations/${id}`);
+    if (res.ok) {
+        
+        
+        const singleLocation = await res.json()
+        
+        dispatch(getOneLocationAction(singleLocation))
+        return singleLocation
+    }
+
+}
+
+
+
+// reducerville
+const initialState = {}
+
+const locationReducer = ( state = initialState, action) => {
+
+    let newState = {};
+
+    switch (action.type) {
+
+        case ALL_LOCATIONS: {
+            action.payload.locations.forEach(location => {
+                newState[location.id] = location
+            })
+            return newState
+        }
+
+        case ONE_LOCATION: {
+
+            newState = {...state };
+            newState[action.payload.id] = action.payload;
+
+            return newState
+            
+        }
+
+        default: {
+            return state;
+        }
+    }}
+
+
+    export default locationReducer;

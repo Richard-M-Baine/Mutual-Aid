8131085f6ca8182ba54989c172995567841d0d64
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-11-24 16:28:27 -0500

    give up with edit for time being

--- react-app/src/components/groups/editGroup/index.js
+++ react-app/src/components/groups/editGroup/index.js
@@ -11,13 +11,25 @@ import {getOneLocationThunk} from '../../../store/locations'
 
 function EditCharityForm(){
 
-    
-    
-    
-    
+    const {id} = useParams()
 
-    return (
-        <div>blah</div>
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    const [loaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        dispatch(getOneGroupThunk(id)).then(() => setIsLoaded(true))       
+    }, [dispatch])
+
+    useEffect(() => {
+        dispatch(getOneLocationThunk(id)).then(() => setIsLoaded(true))       
+    }, [dispatch])
+
+    return loaded && (
+        <div>
+            <label>blah</label>
+        </div>
     )
 }
 

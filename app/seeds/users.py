09c811c6896593a0b6156ffcebc80ef0510ab76d
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-11-25 18:17:52 -0500

    added first last name for signup

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,11 +4,11 @@ from app.models import db, User, environment, SCHEMA
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
+        firstName='Bilbo', lastName='Baggins', username='bilbo1409', email='bilbo@gmail.com', password='password')
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+        firstName='Gandalf', lastName='Gamgee', username='second', email='marnie@aa.io', password='password')
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        firstName='Smeagol', lastName='Gollum', username='third', email='bobbie@aa.io', password='password')
 
     db.session.add(demo)
     db.session.add(marnie)

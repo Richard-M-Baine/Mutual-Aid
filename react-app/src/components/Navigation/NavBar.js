39b2ce4dbe5890560f5b2c1213f4844c46694414
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-11-25 17:54:47 -0500

    have functioning navbar!

--- react-app/src/components/Navigation/NavBar.js
+++ react-app/src/components/Navigation/NavBar.js
@@ -2,42 +2,75 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
+import { useSelector } from 'react-redux';
 
 import './navigation.css'
 
-const NavBar = () => {
+const NavBar = ({ loaded }) => {
 
+  const sessionUser = useSelector(state => state.session.user);
 
+  let sessionLinks;
 
-  
-  return (
-    <nav className='navigationOuterDiv'>
-      
+  if (sessionUser) {
+    sessionLinks = (
+      <nav className='navigationOuterDiv'>
         <div className='navigationLogInDiv'>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
+          <NavLink to='/mylistings' exact={true} activeClassName='active'>
+            My Home Page
           </NavLink>
         </div>
-        <div >
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </div>
+
         <div>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </div>
-        <div>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
+          <NavLink to='/newgroup' exact={true} activeClassName='active'>
+            Create a Group
           </NavLink>
         </div>
         <div className='navigationLogInDiv'>
           <LogoutButton />
         </div>
-      
-    </nav>
+      </nav>
+
+    )
+  }
+
+
+  else {
+
+    sessionLinks=(
+    <nav className='navigationOuterDiv'>
+
+    <div className='navigationLogInDiv'>
+      <NavLink to='/' exact={true} activeClassName='active'>
+        Home
+      </NavLink>
+    </div>
+    <div >
+      <NavLink to='/login' exact={true} activeClassName='active'>
+        Login
+      </NavLink>
+    </div>
+    <div>
+      <NavLink to='/sign-up' exact={true} activeClassName='active'>
+        Sign Up
+      </NavLink>
+    </div>
+    <div>
+      <NavLink to='/users' exact={true} activeClassName='active'>
+        Users
+      </NavLink>
+    </div>
+    
+  </nav>
+    )
+
+
+  }
+
+  return (
+    <div>
+    {loaded && sessionLinks}
+    </div>
   );
 }
 

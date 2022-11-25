99f70cea9bfe7da59a6ed09a31d530a77dcb8f71
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-11-25 16:42:11 -0500

    some progress stuck on edit

--- react-app/src/components/groups/editGroup/index.js
+++ react-app/src/components/groups/editGroup/index.js
@@ -12,26 +12,42 @@ import {getOneLocationThunk} from '../../../store/locations'
 function EditCharityForm(){
 
     const {id} = useParams()
+    
+    const group = useSelector((state) => state?.groups);
+    
+    const figureOne = group[id]?.about
+    console.log(figureOne)
+    
 
     const history = useHistory();
     const dispatch = useDispatch();
 
+    const [about, setAbout] = useState(group[id]?.about)
     const [loaded, setIsLoaded] = useState(false)
+    const [second, setSecond] = useState(false)
+    
+    console.log('i am about ',about)
 
     useEffect(() => {
         dispatch(getOneGroupThunk(id)).then(() => setIsLoaded(true))       
     }, [dispatch])
 
     useEffect(() => {
-        dispatch(getOneLocationThunk(id)).then(() => setIsLoaded(true))       
+        dispatch(getOneLocationThunk(id)).then(() => setSecond(true))       
     }, [dispatch])
 
-    return loaded && (
+    return loaded && second && (
         <div>
             <form>
         <div>
             <label>name</label>
-            <input />
+            <input
+            type='text' 
+            value={about}
+            
+            onChange={e => setAbout(e.target.value)}
+            required
+            />
         </div>
 
         <div>

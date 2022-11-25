9c11613532c09277cc71f8a1b107795f00954791
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-11-25 17:24:38 -0500

    little steps

--- react-app/src/components/Navigation/NavBar.js
+++ react-app/src/components/Navigation/NavBar.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 
+import './navigation.css'
+
 const NavBar = () => {
 
 
@@ -10,31 +12,31 @@ const NavBar = () => {
   
   return (
     <nav>
-      <ul>
-        <li>
+      
+        <div>
           <NavLink to='/' exact={true} activeClassName='active'>
             Home
           </NavLink>
-        </li>
-        <li>
+        </div>
+        <div>
           <NavLink to='/login' exact={true} activeClassName='active'>
             Login
           </NavLink>
-        </li>
-        <li>
+        </div>
+        <div>
           <NavLink to='/sign-up' exact={true} activeClassName='active'>
             Sign Up
           </NavLink>
-        </li>
-        <li>
+        </div>
+        <div>
           <NavLink to='/users' exact={true} activeClassName='active'>
             Users
           </NavLink>
-        </li>
-        <li>
+        </div>
+        <div>
           <LogoutButton />
-        </li>
-      </ul>
+        </div>
+      
     </nav>
   );
 }

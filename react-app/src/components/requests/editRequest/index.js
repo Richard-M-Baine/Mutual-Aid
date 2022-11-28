939ef3defeb7402f0e7bcac06a4cde7bb9ad3e5c
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-11-28 12:13:48 -0500

    more progress

--- react-app/src/components/requests/editRequest/index.js
+++ react-app/src/components/requests/editRequest/index.js
@@ -5,16 +5,36 @@ import { useParams, useHistory } from 'react-router-dom';
 
 
 // thunks
-
+import {getOneRequestThunk} from '../../../store/requests.js'
+import { editRequestThunk } from '../../../store/requests.js';
 
 import './editRequest.css'
 
 
 function EditRequestForm() {
+    const history = useHistory()
+    const { id } = useParams()
+
+    const request = useSelector((state) => state?.requests)
+
+    const dispatch = useDispatch();
+
+    const [address, setAddress] = useState(request[id]?.address)
+    const [city, setCity] = useState(request[id]?.city)
+    const [details, setDetails] = useState(request[id]?.details)
+    const [end_time, setEndTime] = useState(request[id]?.end_time)
+    const [start_time, setStartTime] = useState(request[id]?.start_time)
+    const [statee, setStatee] = useState(request[id]?.state)
+    const [title, setTitle] = useState(request[id]?.title)
+    const [loaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        
+        dispatch(getOneRequestThunk(id)).then(() => setIsLoaded(true))
+    }, [dispatch])
 
 
-
-    return (
+    return loaded && (
         <h1>blah</h1>
     )
 }

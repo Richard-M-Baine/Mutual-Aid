daedbb2cbbf769a444f729c5c755309f7437af64
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-11-28 21:31:43 -0500

    another attempt

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -24,12 +24,14 @@ def seed():
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_requests()
-        undo_locations()
         undo_groups()
+        undo_locations()
         undo_users()
+        
+        
     seed_users()
-    seed_groups()
     seed_locations()
+    seed_groups()
     seed_requests()
     # Add other seed functions here
 

c53777fb7213acdab5a7069b3d3c3ba1a89203e9
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-11-30 14:55:51 -0500

    styling for all requests better

--- react-app/src/components/requests/allRequests/RequestCard/index.js
+++ react-app/src/components/requests/allRequests/RequestCard/index.js
@@ -2,18 +2,56 @@ import { NavLink } from 'react-router-dom';
 import React, { useState, useEffect } from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 
-
+import './requestcard.css'
 
 function RequestCard({request}) {
     
+    let date = request?.start_time?.slice(5, 7)
+    let day = request?.start_time?.slice(0, 3)
+    let month = request?.start_time?.slice(8, 11)
+    let hour = request?.start_time?.slice(17, 19)
+    let minute = request?.start_time?.slice(20, 22)
+    let zeit
+    if (hour > 12) {
+        zeit = `${hour - 12}:${minute} PM`
+    }
+    else {
+        zeit = `${hour}:${minute} AM`
+    }
+
+    let endHour = request?.end_time?.slice(17, 19)
+    let endMinute = request?.end_time?.slice(20, 22)
+    let endZeit
+    if (endHour > 12) {
+        endZeit = `${endHour - 12}:${endMinute} PM`
+    }
+    else {
+        endZeit = `${endHour}:${endMinute} AM`
+    }
+
+    let suffix
+    if (date ==='01'){
+        suffix = 'st'
+    }
+    else if (date ==='02'){
+        suffix = 'nd'
+    }
+    else if (date ==='03'){
+        suffix = 'rd'
+    }
+    else{
+        suffix = 'th'
+    }
     
 
 
     return (
-        <NavLink className='navGroupAll' to={`/requests/${request.id}`}> 
-        <h3>{request.details}</h3>
-        <h5>{request.start_time}</h5>
-        <h5>{request.end_time}</h5>
+        <NavLink className='navReqestCardAllRequests' to={`/requests/${request.id}`}>
+        <h3>{request.address} - {request.city} {request.state}</h3>
+        <h3>Date of request {day} {month} {date}{suffix} at {zeit}</h3>
+        <h3>Request will end at {endZeit}</h3>     
+        <h3 className='detailsRequestAllCard'>{request.details}</h3>
+       
         
         </NavLink>
 
--- react-app/src/components/requests/allRequests/index.js
+++ react-app/src/components/requests/allRequests/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import {useDispatch, useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import RequestCard from './RequestCard/index.js'
 
@@ -7,47 +7,49 @@ import RequestCard from './RequestCard/index.js'
 
 import { fetchAllRequestsThunk } from '../../../store/requests'
 
+import './allRequests.css'
 
-function AllRequests(){
+
+function AllRequests() {
     const dispatch = useDispatch()
     const requests = useSelector(state => state.requests)
 
 
-    
-    const requestList = Object.values(requests)
-        
 
-    
-     
-    
+    const requestList = Object.values(requests)
+
+
+
+
+
 
     const [loaded, setLoaded] = useState(false)
 
     useEffect(() => {
         dispatch(fetchAllRequestsThunk())
-        .then(() => setLoaded(true))
-    },[dispatch])
-    
+            .then(() => setLoaded(true))
+    }, [dispatch])
+
     console.log(requestList)
     return loaded && (
-    <div className='main'>
-       
-             <div className='groupPart'>
-                <div className='groupTextDiv'>
+        <div className='mainAllRequests'>
+
+            <div className='requestPart'>
+                <div className='requestTextDiv'>
                     <h1> All Requests</h1>
                 </div>
 
-                <div className='groupsAllPart'>
-            {requestList.map(request => (
-                <RequestCard request={request} key={request.id}/>
-            ))}
+                <div className='requestAllPart'>
+                    {requestList.map(request => (
+                        <RequestCard request={request} key={request.id} />
+                    ))}
                 </div>
-                
-           
+
+
+            </div>
         </div>
-    </div>
     )
-    
+
 }
 
 export default AllRequests;

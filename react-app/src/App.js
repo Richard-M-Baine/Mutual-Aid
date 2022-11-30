98660bd0b036dbcaee4dbddb29fa2ab848f62a9b
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-11-30 13:25:42 -0500

    users is gone

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -98,13 +98,6 @@ function App() {
           < MyCharities />
         </ProtectedRoute>
 
-        <ProtectedRoute path='/users' exact={true} >
-          <UsersList />
-        </ProtectedRoute>
-
-        <ProtectedRoute path='/users/:userId' exact={true} >
-          <User />
-        </ProtectedRoute>
 
 
 

c24fcbafce905d80dcae60eca169fbd66424a201
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-12-02 12:58:38 -0500

    altered stuff

--- react-app/src/components/HomePage/about/index.js
+++ react-app/src/components/HomePage/about/index.js
@@ -9,7 +9,11 @@ function About() {
         history.push('/')
     }
 
+    const [part, setPart] = useState('Part One')
+
     return (
+
+    
         <div className='backgroundcoloraboutdiv'>
             <div className='aboutmutualaidoutdiv'>
             <button className='topaboutgohomebutton' autofocus onClick={goHome}>Back To Home</button>
@@ -26,8 +30,10 @@ function About() {
                         Your chances were far greater if you were a member of a tribal community.  Birds who stayed put for the winter would have
                         to little food for everyone to survive.  For this reason the majority migrated to warmer climates.  During this process they would cooperate while flying.  Taking turns in front of a V “formation” the birds in the rear could rest by drafting.  The same process as in automobile racing or competitive cycling.  </p>
                 </div>
+
+
                 <div className='intropartaboutdiv'>
-                    <h3 className='headerthreeabout'>So is this another charity?</h3>
+                    <h2 className='headerthreeabout'>So is this another charity?</h2>
                     <p className='paragraphaboutdiv1'>Kropotkin later became what was later known as an Anarchist and his political beliefs are what he is more well known about today.  A belief that differs from communism in that it believes that power disparities, both government and corporate based, need to be flattened.  Many charitable organizations have chains of command just as elaborate as Apple, Boeing and other corporations.  The people on the top naturally receive the most perks.  They also have a significant role in playing who deserves and who do not deserve assistance.  You can understand that when it comes to housing, food, or financial assistance their own personal opinions can outplay any objective reasoning.     </p>
                     <p className='paragraphaboutdiv1'>Mutual aid thus borrows the term from Kropotkin but also embrace his anarchist credentials.  Instead of a charity influencing who deserves, and who doesn’t deserve assistance, mutual aid believes in the concept of “people directly helping people.”  A classic example is Food not Bombs, a loosely based federation of independent pop-up food pantries.  Anyone can volunteer and anyone can receive food.  No questions asked.  Sikhism, and Sufism, have a similar process called Langar.  Compared to related charities decisions are governed by consensus and the scope is comparatively local.      </p>
                     <p className='paragraphaboutdiv1'>Rationalism dictates that charitable organizations exist and will continue to exist for the indefinite future.  The creator of this site has worked in the non-profit industry his entire professional career.  Thus up to date listings can enable individuals to locate services in their community.  The current listings will be transformed into a wiki style format enabling up to date information to be presented and edited at will.  There are similar listings on other sites but naturally if governed by one individual they tend to become inactive after a while.       </p>

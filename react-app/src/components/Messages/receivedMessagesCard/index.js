5b097065885caf056400f4611f55661b4df670c8
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-12-20 15:59:43 -0500

    modal populates in response

--- react-app/src/components/Messages/receivedMessagesCard/index.js
+++ react-app/src/components/Messages/receivedMessagesCard/index.js
@@ -1,21 +1,22 @@
-import { NavLink, useHistory} from 'react-router-dom';
-import React, {useEffect, useState} from 'react'
-import { useDispatch, useSelector} from 'react-redux'
+import { NavLink, useHistory } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 import './receivedMessages.css'
+import RespondToUserMessageModal from './RespondToUserMessageModal/index.js'
 
 // thunks
-import {markReadThunk} from '../../../store/messages'
+import { markReadThunk } from '../../../store/messages'
 
-function MyReceivedMessageCard({receivedMessage}){
+function MyReceivedMessageCard({ receivedMessage }) {
 
     const history = useHistory()
     const dispatch = useDispatch()
-    
+
     const markMessageRead = e => {
         e.preventDefault()
         dispatch(markReadThunk(receivedMessage?.id))
-        .then(() => history.push('/mylistings'))
+            .then(() => history.push('/mylistings'))
     }
 
 
@@ -23,8 +24,10 @@ function MyReceivedMessageCard({receivedMessage}){
         <div className='sentMessageCardMainDiv'>
             <div className='sentMessageCardRecipient'> sent by {receivedMessage?.sender}</div>
             <div className='sentMessageCardBody'>{receivedMessage?.body}</div>
-            <button className='sentMessageCardDelete' onClick={markMessageRead}>mark read</button>
-
+            <div>
+                <button className='sentMessageCardDelete' onClick={markMessageRead}>Delete Message</button>
+                <div><RespondToUserMessageModal receivedMessage={receivedMessage} key={receivedMessage?.id} /></div>
+            </div>
         </div>
     )
 }

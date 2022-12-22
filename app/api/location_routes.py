b35183246d1b952a8e351bb8c6c1efbd78b2c2f5
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-12-22 18:23:17 -0500

    added api key and first google map route

--- app/api/location_routes.py
+++ app/api/location_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, render_template, redirect, request, make_response
 from flask_login import login_required,current_user
+import os
 
 
 from datetime import datetime
@@ -78,3 +79,10 @@ def delete_location(id):
         "statusCode": 200
         }
             
+# google map api call area fitting area
+
+@location_routes.route('/key', methods=['POST'])
+@login_required
+def load_map_key():
+    key = os.environ.get('MAPS_API_KEY')
+    return {'googleMapsAPIKey': key}

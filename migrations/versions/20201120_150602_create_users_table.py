b1ea9636ea592aee54ace0f8ebfc3cebc95b1d1f
Richard-M-Baine <efdeliveryservice@gmail.com>
2023-01-04 17:30:12 -0500

    experiment number 1500025

--- migrations/versions/20201120_150602_create_users_table.py
+++ migrations/versions/20201120_150602_create_users_table.py
@@ -100,6 +100,7 @@ def upgrade():
 
     if environment == "production":
         op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE locations SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###qqqqqqqqq
 
 
